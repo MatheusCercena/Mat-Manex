e7420d4aebf96a231b2ab52518c262a34046a218
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-03-12 20:20:21 -0300

    mudando listas

--- scripts/menu.py
+++ scripts/menu.py
@@ -24,30 +24,33 @@ while opt not in options:
     if opt not in options:
         print('Opção inválida! Digite um número de 1 a 5: ')
 
-lista_de_receitas = []
-lista_de_gastos = []
 
-if opt == 1:
-    while True:
-        lista_de_receitas.append(cadastrar_receita())
-        res = input('Aperte enter para cadastrar outra receita ou qualquer outra tecla para parar.')
-        if res != '':
-            break
-    cadastrar_receita()
-elif opt == 2:
-    while True:
-        lista_de_gastos.append(cadastrar_gasto())
-        res = input('Aperte enter para cadastrar outro gasto ou qualquer outra tecla para parar.')
-        if res != '':
-            break
-elif opt == 3:
-    # ver_lista()
-    for receita in lista_de_receitas:
-        print(f'Valor: {receita['valor']} - Categoria: {receita['categ']} - Pagante: {receita['pag']} - Observações: {receita['obs']}')
-    for gasto in lista_de_gastos:
-        print(f'Valor: {gasto['valor']}\n Categoria: {gasto['categ']} \n Favorecido: {gasto['fav']} \n Observações: {gasto['obs']}')
+lista_de_transações = []
+# lista_de_receitas = []
+# lista_de_gastos = []
 
-# elif opt == 4:
-#     ver_estatisticas()
-else:
-    sys.exit   
+# if opt == 1:
+#     while True:
+#         lista_de_receitas.append(cadastrar_receita())
+#         res = input('Aperte enter para cadastrar outra receita ou qualquer outra tecla para parar.')
+#         if res != '':
+#             break
+#     cadastrar_receita()
+# elif opt == 2:
+#     while True:
+#         lista_de_gastos.append(cadastrar_gasto())
+#         res = input('Aperte enter para cadastrar outro gasto ou qualquer outra tecla para parar.')
+#         if res != '':
+#             break
+# elif opt == 3:
+#     # ver_lista()
+    
+#     for receita in lista_de_receitas:
+#         print(f'Valor: {receita['valor']} - Categoria: {receita['categ']} - Pagante: {receita['pag']} - Observações: {receita['obs']}')
+#     for gasto in lista_de_gastos:
+#         print(f'Valor: {gasto['valor']}\n Categoria: {gasto['categ']} \n Favorecido: {gasto['fav']} \n Observações: {gasto['obs']}')
+
+# # elif opt == 4:
+# #     ver_estatisticas()
+# else:
+#     sys.exit   
--- scripts/menu_functions.py
+++ scripts/menu_functions.py
@@ -1,12 +1,48 @@
-def cadastrar_receita():
-    receita = {}
-    receita['valor'] = float(input('Valor: '))
-    receita['categ'] = input('Categoria: ')
-    receita['pag'] = input('Pagante: ')
-    receita['obs'] = input('Observações: ')
+# def cadastrar_receita():
+#     receita = {}
+#     receita['valor'] = float(input('Valor: '))
+#     receita['categ'] = input('Categoria: ')
+#     receita['pag'] = input('Pagante: ')
+#     receita['obs'] = input('Observações: ')
 
-    print(f'Receita cadastrada com sucesso. \n Valor: {receita['valor']}\n Categoria: {receita['categ']} \n Pagante: {receita['pag']} \n Observações: {receita['obs']}')
-    return(receita.copy())
+#     print(f'Receita cadastrada com sucesso. \n Valor: {receita['valor']}\n Categoria: {receita['categ']} \n Pagante: {receita['pag']} \n Observações: {receita['obs']}')
+#     return(receita.copy())
+    
+# def cadastrar_gasto():
+#     gasto = {}
+#     gasto['valor'] = float(input('Valor: '))
+#     gasto['categ'] = input('Categoria: ')
+#     gasto['fav'] = input('Favorecido: ')
+#     gasto['obs'] = input('Observações: ')
+
+#     print(f'Gasto cadastrado com sucesso. \n Valor: {gasto['valor']}\n Categoria: {gasto['categ']} \n Favorecido: {gasto['fav']} \n Observações: {gasto['obs']}')
+#     return(gasto.copy())
+
+from datetime import date
+
+def cadastrar_transação(a):
+    data_padrao = date.today().strftime('%d/%m/%Y')
+    if a == 0:
+        transações = {}
+        transações['tipo'] = 'recebimento'
+        transações['data'] = input(f'Data: tecle enter para {data_padrao}:')
+        transações['valor'] = float(input('Valor: '))
+        transações['categ'] = input('Categoria: ')
+        transações['orig'] = input('Pagante: ')
+        transações['obs'] = input('Observações: ')
+        print(f'Receita cadastrada com sucesso. \nTipo: {transações['tipo']} \nData: {transações['data']} \nValor: {transações['valor']} \nCategoria: {transações['categ']} \nPagante: {transações['orig']} \nObservações: {transações['obs']}')
+
+    if a == 1:
+        transações = {}
+        transações['tipo'] = 'pagamento'
+        transações['data'] = input(f'Data: tecle enter para {data_padrao}:')
+        transações['valor'] = float(input('Valor: '))
+        transações['categ'] = input('Categoria: ')
+        transações['orig'] = input('Favorecido: ')
+        transações['obs'] = input('Observações: ')
+        print(f'Gasto cadastrada com sucesso. \nTipo: {transações['tipo']} \nData: {transações['data']} \nValor: {transações['valor']} \nCategoria: {transações['categ']} \nFavorecido: {transações['orig']} \nObservações: {transações['obs']}')
+
+    return(transações.copy())
     
 def cadastrar_gasto():
     gasto = {}
@@ -17,6 +53,8 @@ def cadastrar_gasto():
 
     print(f'Gasto cadastrado com sucesso. \n Valor: {gasto['valor']}\n Categoria: {gasto['categ']} \n Favorecido: {gasto['fav']} \n Observações: {gasto['obs']}')
     return(gasto.copy())
+
+
 
 # def ver_lista():
     
