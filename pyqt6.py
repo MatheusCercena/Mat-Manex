839a8cea7fa8f5ee73ac62087730dafeaee968c5
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-03-25 08:10:38 -0300

    Create PyQt6

---
+++ pyqt6.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QTextEdit, QVBoxLayout
+from PyQt6.QtGui import QIcon
+
+app = QApplication()
+
+sys.exit(app.exec_())
+
+#min 5:00 https://youtu.be/SelawmXHtPg?list=PL3JVwFmb_BnSOj_OtnKlsc2c7Jcs6boyB&t=299
