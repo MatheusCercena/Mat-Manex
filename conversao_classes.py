3084246eb63ff5d93aee9b53e4b474d5930f40cb
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-04-01 18:50:26 -0300

    ainda converterndo pra classes

--- conversao_classes.py
+++ conversao_classes.py
@@ -1,8 +1,9 @@
 from datetime import date
 from datetime import datetime
+import json
 
 class Transação:
-    def __init__(self, id, tipo, data, valor, categoria, origem, observaçoes):
+    def __init__(self, tipo, data, valor, categoria, origem, observaçoes):
         self.id = None
         self.tipo = tipo
         self.data = self.validar_data(data)
@@ -21,7 +22,8 @@ class Transação:
             'origem' : self.origem,
             'obs' : self.observaçoes
         }  
-        
+    
+    @staticmethod
     def validar_data(data):
         data_padrao = date.today().strftime('%d/%m/%Y')
         ano_atual = date.today().year
@@ -39,6 +41,7 @@ class Transação:
         print('Data inválida.')
         return data_padrao
 
+    @staticmethod
     def solicitar_float(mensagem):
         while True: 
             n = input(mensagem.strip())
@@ -49,8 +52,39 @@ class Transação:
                 print('Valor inválido')
 
 class Cadastro:
-    def __init__(self):
-        pass
+    def __init__(self, arquivo_json = 'data.json'):
+        self.arquivo_json = arquivo_json
+        self.lista_de_transações = self.carregar_transações()
 
+    def carregar_transações(self):
+        try:
+            with open(self.arquivo_json, "r") as file:
+                transações = json.load(file)
+                for t in transações:
+                    return Transação(**t) 
+                        
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
 
-#...
+    def salvar_transações(self):
+        with open(self.arquivo_json, "w") as file:
+            json.dump([t.to_dict() for t in self.lista_de_transações], file)
+
+    def cadastrar_transação(self, tipo):
+        data = input('Data (Enter para hoje): ')
+        valor = input('Valor: ')
+        categoria = input('Categoria: ')
+        origem = input('Pagante/Favorecido: ')
+        observacoes = input('Observações: ')
+
+        transação = Transação(self, tipo, data, valor, categoria, origem, observacoes)#cria um objeto da classe Transação, em que seus atributos podem ser acessados pela notação: objeto.atributo
+        transação.id = len(self.lista_de_transações) + 1
+        self.lista_de_transações.append(transação)
+        self.salvar_transações()
+        print(f'{tipo} cadastrado com sucesso!')
+
+    def ver_lista(self):
+        print('\nLista de transações: \n')
+        print()
+        for transação in self.lista_de_transações:
+            print(f'ID: {transação.id:3} Tipo: {transação['tipo']:10} Data: {transação['data']:11} Valor: {transação['valor']:8} Categoria: {transação['categoria']:20} Favorecido: {transação['origem']:20} \nObservações: {transação['obs']:20}')
--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt6.QtWidgets import QApplication 
-from pyqt6 import main_window
+from main_gui import main_window
 
 app = QApplication(sys.argv)
 
 main_window.show()
 
 app.exec()
+
+
 
